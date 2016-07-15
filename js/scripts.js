aaffb6218a98725733aea3497d0471923e229ee7
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-07-15 19:41:42 +0100

    Some ES6

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
-require('./module1.js');
-require('./module2.js');
+function vol( {length:l, width:w, height:h, callback:c} ) {
+	var vol = l * w * h;
+
+	if (c) {
+		c(vol);
+	}
+
+	return vol;
+}
+
+console.log( vol({length:2, width:3, height:5, callback: function (vol){
+	console.log("This is a callback for " + vol);
+} }) );
+
+var name = "bolt";
+console.log(`I like ${name}`); // using backtick for templating
+
+
